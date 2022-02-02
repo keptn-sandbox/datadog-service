40ed5cb81a0db141fe11090af201dc19ee334d08
Suraj Banakar <surajrbanakar@gmail.com>
2022-02-02 12:06:19 +0530

    refactor: remove handlers which are not required

--- eventhandler_test.go
+++ eventhandler_test.go
@@ -3,9 +3,10 @@ package main
 import (
 	"encoding/json"
 	"fmt"
-	"github.com/keptn/go-utils/pkg/lib/v0_2_0/fake"
 	"io/ioutil"
 	"testing"
+
+	"github.com/keptn/go-utils/pkg/lib/v0_2_0/fake"
 
 	keptn "github.com/keptn/go-utils/pkg/lib/keptn"
 	keptnv2 "github.com/keptn/go-utils/pkg/lib/v0_2_0"
@@ -37,86 +38,6 @@ func initializeTestObjects(eventFileName string) (*keptnv2.Keptn, *cloudevents.E
 	myKeptn, err := keptnv2.NewKeptn(incomingEvent, keptnOptions)
 
 	return myKeptn, incomingEvent, err
-}
-
-// Tests HandleActionTriggeredEvent
-// TODO: Add your test-code
-func TestHandleActionTriggeredEvent(t *testing.T) {
-	myKeptn, incomingEvent, err := initializeTestObjects("test-events/action.triggered.json")
-	if err != nil {
-		t.Error(err)
-		return
-	}
-
-	specificEvent := &keptnv2.ActionTriggeredEventData{}
-	err = incomingEvent.DataAs(specificEvent)
-	if err != nil {
-		t.Errorf("Error getting keptn event data")
-	}
-
-	err = HandleActionTriggeredEvent(myKeptn, *incomingEvent, specificEvent)
-	if err != nil {
-		t.Errorf("Error: " + err.Error())
-	}
-
-	gotEvents := len(myKeptn.EventSender.(*fake.EventSender).SentEvents)
-
-	// Verify that HandleGetSliTriggeredEvent has sent 2 cloudevents
-	if gotEvents != 2 {
-		t.Errorf("Expected two events to be sent, but got %v", gotEvents)
-	}
-
-	// Verify that the first CE sent is a .started event
-	if keptnv2.GetStartedEventType(keptnv2.ActionTaskName) != myKeptn.EventSender.(*fake.EventSender).SentEvents[0].Type() {
-		t.Errorf("Expected a action.started event type")
-	}
-
-	// Verify that the second CE sent is a .finished event
-	if keptnv2.GetFinishedEventType(keptnv2.ActionTaskName) != myKeptn.EventSender.(*fake.EventSender).SentEvents[1].Type() {
-		t.Errorf("Expected a action.finished event type")
-	}
-}
-
-// Tests HandleDeploymentTriggeredEvent
-// TODO: Add your test-code
-func TestHandleDeploymentTriggeredEvent(t *testing.T) {
-	myKeptn, incomingEvent, err := initializeTestObjects("test-events/evaluation.triggered.json")
-	if err != nil {
-		t.Error(err)
-		return
-	}
-
-	specificEvent := &keptnv2.DeploymentTriggeredEventData{}
-	err = incomingEvent.DataAs(specificEvent)
-	if err != nil {
-		t.Errorf("Error getting keptn event data")
-	}
-
-	err = HandleDeploymentTriggeredEvent(myKeptn, *incomingEvent, specificEvent)
-	if err != nil {
-		t.Errorf("Error: " + err.Error())
-	}
-}
-
-// Tests HandleEvaluationTriggeredEvent
-// TODO: Add your test-code
-func TestHandleEvaluationTriggeredEvent(t *testing.T) {
-	myKeptn, incomingEvent, err := initializeTestObjects("test-events/evaluation.triggered.json")
-	if err != nil {
-		t.Error(err)
-		return
-	}
-
-	specificEvent := &keptnv2.EvaluationTriggeredEventData{}
-	err = incomingEvent.DataAs(specificEvent)
-	if err != nil {
-		t.Errorf("Error getting keptn event data")
-	}
-
-	err = HandleEvaluationTriggeredEvent(myKeptn, *incomingEvent, specificEvent)
-	if err != nil {
-		t.Errorf("Error: " + err.Error())
-	}
 }
 
 // Tests the HandleGetSliTriggeredEvent Handler
@@ -154,26 +75,5 @@ func TestHandleGetSliTriggered(t *testing.T) {
 	// Verify that the second CE sent is a .finished event
 	if keptnv2.GetFinishedEventType(keptnv2.GetSLITaskName) != myKeptn.EventSender.(*fake.EventSender).SentEvents[1].Type() {
 		t.Errorf("Expected a get-sli.finished event type")
-	}
-}
-
-// Tests the HandleReleaseTriggeredEvent Handler
-// TODO: Add your test-code
-func TestHandleReleaseTriggeredEvent(t *testing.T) {
-	myKeptn, incomingEvent, err := initializeTestObjects("test-events/release.triggered.json")
-	if err != nil {
-		t.Error(err)
-		return
-	}
-
-	specificEvent := &keptnv2.ReleaseTriggeredEventData{}
-	err = incomingEvent.DataAs(specificEvent)
-	if err != nil {
-		t.Errorf("Error getting keptn event data")
-	}
-
-	err = HandleReleaseTriggeredEvent(myKeptn, *incomingEvent, specificEvent)
-	if err != nil {
-		t.Errorf("Error: " + err.Error())
 	}
 }
--- eventhandlers.go
+++ eventhandlers.go
@@ -12,82 +12,10 @@ import (
 
 	"github.com/DataDog/datadog-api-client-go/api/v1/datadog"
 	cloudevents "github.com/cloudevents/sdk-go/v2" // make sure to use v2 cloudevents here
-	keptn "github.com/keptn/go-utils/pkg/lib"
 	keptnv2 "github.com/keptn/go-utils/pkg/lib/v0_2_0"
 )
 
-/**
-* Here are all the handler functions for the individual event
-* See https://github.com/keptn/spec/blob/0.8.0-alpha/cloudevents.md for details on the payload
-**/
-
-// GenericLogKeptnCloudEventHandler is a generic handler for Keptn Cloud Events that logs the CloudEvent
-func GenericLogKeptnCloudEventHandler(myKeptn *keptnv2.Keptn, incomingEvent cloudevents.Event, data interface{}) error {
-	log.Printf("Handling %s Event: %s", incomingEvent.Type(), incomingEvent.Context.GetID())
-	log.Printf("CloudEvent %T: %v", data, data)
-
-	return nil
-}
-
-// OldHandleConfigureMonitoringEvent handles old configure-monitoring events
-// TODO: add in your handler code
-func OldHandleConfigureMonitoringEvent(myKeptn *keptnv2.Keptn, incomingEvent cloudevents.Event, data *keptn.ConfigureMonitoringEventData) error {
-	log.Printf("Handling old configure-monitoring Event: %s", incomingEvent.Context.GetID())
-
-	return nil
-}
-
-// HandleConfigureMonitoringTriggeredEvent handles configure-monitoring.triggered events
-// TODO: add in your handler code
-func HandleConfigureMonitoringTriggeredEvent(myKeptn *keptnv2.Keptn, incomingEvent cloudevents.Event, data *keptnv2.ConfigureMonitoringTriggeredEventData) error {
-	log.Printf("Handling configure-monitoring.triggered Event: %s", incomingEvent.Context.GetID())
-
-	return nil
-}
-
-// HandleDeploymentTriggeredEvent handles deployment.triggered events
-// TODO: add in your handler code
-func HandleDeploymentTriggeredEvent(myKeptn *keptnv2.Keptn, incomingEvent cloudevents.Event, data *keptnv2.DeploymentTriggeredEventData) error {
-	log.Printf("Handling deployment.triggered Event: %s", incomingEvent.Context.GetID())
-
-	return nil
-}
-
-// HandleTestTriggeredEvent handles test.triggered events
-// TODO: add in your handler code
-func HandleTestTriggeredEvent(myKeptn *keptnv2.Keptn, incomingEvent cloudevents.Event, data *keptnv2.TestTriggeredEventData) error {
-	log.Printf("Handling test.triggered Event: %s", incomingEvent.Context.GetID())
-
-	return nil
-}
-
-// HandleApprovalTriggeredEvent handles approval.triggered events
-// TODO: add in your handler code
-func HandleApprovalTriggeredEvent(myKeptn *keptnv2.Keptn, incomingEvent cloudevents.Event, data *keptnv2.ApprovalTriggeredEventData) error {
-	log.Printf("Handling approval.triggered Event: %s", incomingEvent.Context.GetID())
-
-	return nil
-}
-
-// HandleEvaluationTriggeredEvent handles evaluation.triggered events
-// TODO: add in your handler code
-func HandleEvaluationTriggeredEvent(myKeptn *keptnv2.Keptn, incomingEvent cloudevents.Event, data *keptnv2.EvaluationTriggeredEventData) error {
-	log.Printf("Handling evaluation.triggered Event: %s", incomingEvent.Context.GetID())
-
-	return nil
-}
-
-// HandleReleaseTriggeredEvent handles release.triggered events
-// TODO: add in your handler code
-func HandleReleaseTriggeredEvent(myKeptn *keptnv2.Keptn, incomingEvent cloudevents.Event, data *keptnv2.ReleaseTriggeredEventData) error {
-	log.Printf("Handling release.triggered Event: %s", incomingEvent.Context.GetID())
-
-	return nil
-}
-
 // HandleGetSliTriggeredEvent handles get-sli.triggered events if SLIProvider == datadog
-// This function acts as an example showing how to handle get-sli events by sending .started and .finished events
-// TODO: adapt handler code to your needs
 func HandleGetSliTriggeredEvent(myKeptn *keptnv2.Keptn, incomingEvent cloudevents.Event, data *keptnv2.GetSLITriggeredEventData) error {
 	log.Printf("Handling get-sli.triggered Event: %s", incomingEvent.Context.GetID())
 
@@ -101,6 +29,9 @@ func HandleGetSliTriggeredEvent(myKeptn *keptnv2.Keptn, incomingEvent cloudevent
 	// Step 2 - Send out a get-sli.started CloudEvent
 	// The get-sli.started cloud-event is new since Keptn 0.8.0 and is required to be send when the task is started
 	_, err := myKeptn.SendTaskStartedEvent(data, ServiceName)
+	if err != nil {
+		log.Printf("err when starting the event: %v", err)
+	}
 
 	start, err := parseUnixTimestamp(data.GetSLI.Start)
 	if err != nil {
@@ -255,50 +186,4 @@ func parseUnixTimestamp(timestamp string) (time.Time, error) {
 	}
 	unix := time.Unix(timestampInt, 0)
 	return unix, nil
-}
-
-// HandleProblemEvent handles two problem events:
-// - ProblemOpenEventType = "sh.keptn.event.problem.open"
-// - ProblemEventType = "sh.keptn.events.problem"
-// TODO: add in your handler code
-func HandleProblemEvent(myKeptn *keptnv2.Keptn, incomingEvent cloudevents.Event, data *keptn.ProblemEventData) error {
-	log.Printf("Handling Problem Event: %s", incomingEvent.Context.GetID())
-
-	// Deprecated since Keptn 0.7.0 - use the HandleActionTriggeredEvent instead
-
-	return nil
-}
-
-// HandleActionTriggeredEvent handles action.triggered events
-// TODO: add in your handler code
-func HandleActionTriggeredEvent(myKeptn *keptnv2.Keptn, incomingEvent cloudevents.Event, data *keptnv2.ActionTriggeredEventData) error {
-	log.Printf("Handling Action Triggered Event: %s", incomingEvent.Context.GetID())
-	log.Printf("Action=%s\n", data.Action.Action)
-
-	// check if action is supported
-	if data.Action.Action == "action-xyz" {
-		// -----------------------------------------------------
-		// 1. Send Action.Started Cloud-Event
-		// -----------------------------------------------------
-		myKeptn.SendTaskStartedEvent(data, ServiceName)
-
-		// -----------------------------------------------------
-		// 2. Implement your remediation action here
-		// -----------------------------------------------------
-		time.Sleep(5 * time.Second) // Example: Wait 5 seconds. Maybe the problem fixes itself.
-
-		// -----------------------------------------------------
-		// 3. Send Action.Finished Cloud-Event
-		// -----------------------------------------------------
-		myKeptn.SendTaskFinishedEvent(&keptnv2.EventData{
-			Status:  keptnv2.StatusSucceeded, // alternative: keptnv2.StatusErrored
-			Result:  keptnv2.ResultPass,      // alternative: keptnv2.ResultFailed
-			Message: "Successfully sleeped!",
-		}, ServiceName)
-
-	} else {
-		log.Printf("Retrieved unknown action %s, skipping...", data.Action.Action)
-		return nil
-	}
-	return nil
 }
